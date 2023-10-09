ef520250048c44aea378335eb4a8d23ff8af78dd
MustafazadeJafar <mi625nkm8@code.edu.az>
2023-10-10 00:36:53 +0400

    Made code more readable + lifted out little perfomance and logic issue.

--- loop_task1.cs
+++ loop_task1.cs
@@ -1,74 +1,64 @@
-﻿// input values
-int givenNumb1 = 1024;
+﻿
+// input values
+int givenNumb1;
+// temporary values
+int temp1;
+bool temp_bool;
 
-for (int j = 0; j < 1026; j++)
+for (int j = -102; j < 1026; j++)
 {
     givenNumb1 = j;
+    temp_bool = true;
 
-    // helping values
-    int temp1 = 0;
-    bool checker = true;
-
-    // 1 2 3 5 7 11 13 17
-
-    // #check if number is prime
-    if (givenNumb1 > 1)
-    {
-        for (int i = 2; i <= givenNumb1 / 2; i++)
-        {
-            if (givenNumb1 % i == 0)
-            {
-                checker = false;
+    // #check if number is prime ////////////////////////////////////////////////////////////////
+    if (givenNumb1 > 1) {
+        for (int i = 2; i <= givenNumb1 / 2; i++) {
+            if (givenNumb1 % i == 0) {
+                temp_bool = false;
                 break;
             }
         }
     }
-    else
-    {
-        checker = false;
+    else {
+        temp_bool = false;
     }
 
-    if (checker)
-    {
-        Console.WriteLine("1.Given number is indeed prime number.");
+    if (temp_bool) {
+        Console.WriteLine("\n1.Given number is indeed prime number.");
     }
-    else
-    {
-        Console.WriteLine("0.Given number is not prime.");
+    else {
+        Console.WriteLine("\n0.Given number is not prime.");
     }
 
 
 
-    // #define if given number is power of 2
+    // #define if given number is power of 2 ////////////////////////////////////////////////////////////////
+    /*
+     normally this kind of number is: 1 and 00000000 like 2 -> 10, 4 -> 100, 8 -> 1000 and etc.
+     what means if we -1 from it it will be: 0 and 111111111111
+     and if we & it with original: 1000 & 0111 -> result always will be 0000.
+    */
+    //temp_bool = ((givenNumb1 & (givenNumb1 - 1)) == 0 && givenNumb1 > 0);
 
-    // normally this kind of number is: 1 and 00000000 like 2 -> 10, 4 -> 100, 8 -> 1000 and etc.
-    // what means if we -1 from it it will be: 0 and 111111111111
-    // and if we & it with original: 1000 & 0111 -> result always will be 0000.
+    temp1 = givenNumb1;
 
-    checker = ((givenNumb1 & (givenNumb1 - 1)) == 0 && givenNumb1 > 0);
+    while (temp1 % 2 == 0 && temp1 != 0) {
+        temp1 /= 2;
+    }
+    temp_bool = (temp1 == 1);
 
-    // here you go loop method also:
-    //temp1 = givenNumb1;
-
-    //while(temp1 % 2 == 0)
-    //{
-    //    temp1 /= 2;
-    //}
-
-    //checker = (temp1 == 1);
-
-    if (checker)
-    {
+    if (temp_bool) {
         Console.WriteLine("1.Given number is indeed power of 2.");
     }
-    else
-    {
+    else {
         Console.WriteLine("0.Given number is not power of 2.");
     }
 
 
 
-    // #calculate level of number
+    // #calculate level of number ////////////////////////////////////////////////////////////////
+    if (givenNumb1 < 0) givenNumb1 *= -1;
+
     temp1 = 0;
     do
     {
@@ -76,6 +66,11 @@ for (int j = 0; j < 1026; j++)
         givenNumb1 /= 10;
     } while (givenNumb1 > 0);
 
-    Console.WriteLine(temp1 + "\t" + j);
-    // more interesting do_while task will be like: check if user input 'good', if not request another input until it 'good'.
+    Console.WriteLine(temp1 + " is level of number -> " + j);
+
+
+
+    for (int i = 0; i < 16; i++) {
+        Console.Write("//");
+    }
 }
