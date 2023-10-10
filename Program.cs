5b183189d2e4d12a0a5cd792c570d571d0743add
MustafazadeJafar <mi625nkm8@code.edu.az>
2023-10-10 21:49:20 +0400

    Added array task(2) + changed whole structure of project.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //loop_task1 task1 = new loop_task1();
+            //task1.Run();
+
+            array_task2 task2 = new array_task2();
+            task2.Run();
+        }
+    }
+}
---
+++ array_task2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorks
+{
+    internal class array_task2
+    {
+        public void Run()
+        {
+            // input values
+            int[] arr1 = { -1, -9, 3, 77 };
+            // temporary values
+            int arr1Len = arr1.Length;
+            int[] arr2 = new int[arr1Len];
+
+            for (int i = 0; i < arr1Len; i++)
+            {
+                arr2[i] = arr1[arr1Len - i - 1];
+            }
+
+            for (int i = 0; i < arr1Len; i++)
+            {
+                Console.Write(arr2[i] + ",");
+            }
+        }
+    }
+}
