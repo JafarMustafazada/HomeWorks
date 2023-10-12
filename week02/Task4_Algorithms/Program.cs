ec296bd14a8dce689f77a2323b7f4b77f047b768
MustafazadeJafar <mi625nkm8@code.edu.az>
2023-10-12 21:11:18 +0400

    + task4 of week02 - Algorithms.

---
+++ week02/Task4_Algorithms/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Fibonacci task
+int numberX = 101;
+int temporary = 0;
+int fibonacci = 1;
+
+while (fibonacci < numberX)
+{
+    Console.Write(fibonacci + ",");
+    fibonacci += temporary;
+    temporary = fibonacci - temporary;
+}
+Console.WriteLine(fibonacci);
+
+
+
+// Swaping task
+int num1 = 14;
+int num2 = 9;
+
+num1 += num2;
+num2 = num1 - num2;
+num1 -= num2;
+
+Console.WriteLine(num1 + " - " + num2);
