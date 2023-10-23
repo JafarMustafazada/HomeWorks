fa4aab1abce014494c8016116c15e66c1a916813
MustafazadeJafar <mi625nkm8@code.edu.az>
2023-10-23 22:39:15 +0400

    + Task 1 of week04 (Access modifers).

---
+++ week04/Task1_AccessModifiers/Models/Products.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_AccessModifiers.Models
+{
+    public class Product
+    {
+        int _count;
+        double _price;
+
+        public double Price
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    this._price = value;
+                }
+                else
+                {
+                    Console.WriteLine("Price error.");
+                }
+            }
+        }
+        public Product(int Count, double Price)
+        {
+            this.Price = Price;
+            this._count = Count;
+        }
+    }
+    public class  NoteBook : Product
+    {
+        string _brand, _model;
+        int _RAM;
+        int _storage;
+
+        public string Brand
+        {
+            set
+            {
+                if (value.Length > 2 && value.Length < 31)
+                {
+                    this._brand = value;
+                }
+                else
+                {
+                    Console.WriteLine("Brand error.");
+                }
+            }
+        }
+        public string Model
+        {
+            set
+            {
+                if (value.Length > 2 && value.Length < 31)
+                {
+                    this._model = value;
+                }
+            }
+        }
+        public int RAM
+        {
+            set
+            {
+                // if value is power of 2, which is more realistic for RAM
+                // and no more than 128:
+                if (value >= 0 && (value - 1 & value) == 0 && value <= 128)
+                {
+                    this._RAM = value;
+                }
+                else
+                {
+                    Console.WriteLine("RAM error.");
+                }
+            }
+        }
+        public int Storage
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    this._storage = value;
+                }
+                else
+                {
+                    Console.WriteLine("Storage error.");
+                }
+            }
+        }
+        public NoteBook(int Count, double Price, string Model) : base(Count, Price)
+        {
+            // model is must be, but brand is no??
+            this.Model = Model;
+        }
+
+        public bool checker()
+        {
+            if (String.IsNullOrWhiteSpace(this._model)) return false;
+            if (String.IsNullOrWhiteSpace(this._brand)) return false;
+            // dose not matter if buyer get RAM or storage >:D
+            // because 0 is acceptable even for price xD
+            return true;
+        }
+    }
+}
---
+++ week04/Task1_AccessModifiers/Program.cs
@@ -0,0 +1,51 @@
+﻿using Task1_AccessModifiers.Models;
+
+namespace Task1_AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // crap interface
+            Console.Write("How many notebooks of doom will be summoned, director Jafar?: ");
+            // Assume, no take it as fact, Jafar cant give other than right value:
+            string input = Console.ReadLine();
+            int HowManyNotes = Convert.ToInt32(input);
+            NoteBook[] notebooks = new NoteBook[HowManyNotes];
+            int PreviusIndex = -1;
+
+            for (int i = 0; i < HowManyNotes; i++)
+            {
+                notebooks[i] = new NoteBook(1,0,"Jhonny+9");
+                Console.WriteLine();
+
+                while (!notebooks[i].checker())
+                {
+                    if (PreviusIndex == i)
+                    {
+                        Console.WriteLine("\nTry again.");
+                    }
+
+                    Console.Write("[" + i + "] Give name of Brand: ");
+                    input = Console.ReadLine();
+                    notebooks[i].Brand = input;
+
+                    Console.Write("[" + i + "] Give RAM capacity: ");
+                    input = Console.ReadLine();
+                    notebooks[i].RAM = Convert.ToInt32(input);
+
+                    Console.Write("[" + i + "] Give storage capacity: ");
+                    input = Console.ReadLine();
+                    notebooks[i].Storage = Convert.ToInt32(input);
+
+                    Console.Write("[" + i + "] Give price: ");
+                    input = Console.ReadLine();
+                    notebooks[i].Price = Convert.ToInt32(input);
+
+                    PreviusIndex = i;
+                }
+            }
+            Console.WriteLine("Done.");
+        }
+    }
+}
